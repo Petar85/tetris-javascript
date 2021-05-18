0757cdb824db3731b85fffb02e83bdea2df9779d
Peter Pan <petarzivkovic85@gmail.com>
2021-05-17 22:28:50 -0700

    created l shape detromino

--- app.js
+++ app.js
@@ -5,5 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn = document.querySelector('#start-button')
     const width = 10
 
+    // Tetrominos
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+    ]
 
 })
