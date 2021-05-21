d7385903d4724003324d6660e076c9b9eab8cb96
Peter Pan <petarzivkovic85@gmail.com>
2021-05-21 16:41:35 -0700

    added squares to incomplete tetromino

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const lTetromino = [
         [1, width+1, width*2+1, 2],
         [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2+2]
+        [width, width*2, width*2+1, width*2+2]
     ]
 
     const zTetromino = [
